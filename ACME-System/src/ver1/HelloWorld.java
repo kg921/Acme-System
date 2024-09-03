e4ef826e5a11fac197daf4ba3e4f72d5c8b713a9
Kathleen Mock <kmock262@gmail.com>
2024-09-03 08:48:08 -0400

    heyNow complete

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -1,10 +1,16 @@
 package ver1;
 
 public class HelloWorld {
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Kathleen is working locally on her branch");
+
 
 	}
 
